aab0391cd44a48a5a4f4c10f56b5376c6c140703
roycesteven <86450052+roycesteven@users.noreply.github.com>
2021-07-13 12:02:05 +0700

    Add files via upload

---
+++ Proyek/src/proyek/Proyek.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyek;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class Proyek {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        NewJFrame_master master = new NewJFrame_master();
+        master.setVisible(true);
+        master.setLocationRelativeTo(null);
+    }
+    
+}
